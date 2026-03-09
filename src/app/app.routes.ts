6ca8166381b2309bfc16269bbb2776a379ecd8ae
walae04 <walae.ouahi@gmail.com>
2026-03-09 11:53:32 +0100

    componnts commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
+import { LoginPage } from './pages/login-page/login-page';
+import { SubscribePage } from './pages/subscribe-page/subscribe-page';
+import { AccountManagerPage } from './pages/account-manager-page/account-manager-page';
+import { RecipeCalculatorPage } from './pages/recipe-calculator-page/recipe-calculator-page';
+import { UsersManagerPage } from './pages/users-manager-page/users-manager-page';
+import { IngredientManagerPage } from './pages/ingredient-manager-page/ingredient-manager-page';
+import { AboutPage } from './pages/about-page/about-page';
+import { RecipeManagerPage } from './pages/recipe-manager-page/recipe-manager-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Accès & redirection vers la page home :
+{ path: '', pathMatch: 'full', redirectTo: 'home' }, // Route de la racine :
+{ path: 'home', component: HomePage },
+// Pages d'authentification :
+{ path: 'login', component: LoginPage },
+{ path: 'subscribe', component: SubscribePage},
+// Compte utilisateur :
+{ path: 'account', component: AccountManagerPage },
+// Recettes :
+{ path: 'recipe-calculator', component: RecipeCalculatorPage },
+{ path: 'recipe-manager', component: RecipeManagerPage},
+// Administration - Gestion :
+{ path: 'users-manager', component: UsersManagerPage },
+{ path: 'ingredients-manager', component: IngredientManagerPage},
+// A propos (redirection vers home en cas d'url invalide):
+{ path: "about", component: AboutPage } // Toujours mis en dernier !
+];
---
+++ src/app/components/footer/footer.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  imports: [ ],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css',
+})
+export class Footer {
+
+}
